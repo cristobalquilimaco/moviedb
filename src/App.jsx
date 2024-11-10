3f2e207a5bce48533479d89b9fa8349311c8b7fc
Cristobal <quilimacox1@gmail.com>
2024-11-10 11:24:22 -0500

    se agregan estados

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import axios from 'axios'
+import { useState } from 'react'
 import YouTube from 'react-youtube'
 
 function App() {
@@ -9,6 +10,10 @@ const API_KEY = "0c8d154b2b93ccc0da064d6ff2a2575b"
 const IMAGE_PATH = "https://image.tmdb.org/t/p/original"
 const URL_IMAGE = "https://image.tmdb.org/t/p/original"
 
+//VARIABLES DE ESTADO
+
+const [movies, setMovies] = useState([])
+const [searchKey, setSearchKey] = useState("")
 
   return (
     <>
