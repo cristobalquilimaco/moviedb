ea92f4134ef645dedc74880e012f3355edb4aa70
Cristobal <quilimacox1@gmail.com>
2024-11-10 13:20:08 -0500

    Se crea fuincion para trailer

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,8 @@ const URL_IMAGE = "https://image.tmdb.org/t/p/original"
 
 //VARIABLES DE ESTADO
 
+
+
 const [movies, setMovies] = useState([]);
 const [searchKey, setSearchKey] = useState("");
 const [trailer, setTrailer] = useState(null);
@@ -33,6 +35,34 @@ const fetchMovies = async(searchKey) =>{
 setMovies(results)
 setMovie(results[0])
 
+if(results.length) {
+  await fetchMovie(results[0].id)
+}
+
+}
+
+
+const fetchMovie = async(id)=>{
+  const {data} = await axios.get(`${API_URL}/movie/${id}`, {
+    params: {
+      api_key: API_KEY,
+      append_to_response: "videos"
+    }
+  })
+
+  if(data.videos && data.videos.results){
+    const trailer = data.videos.results.find(
+      (vid) => vid.name === "Official Trailer"
+    );
+    setTrailer(trailer ? trailer : data.videos.results[0])
+  }
+  setMovie(data)
+} 
+
+const selectMovie = async(movie) =>{
+  fetchMovie(movie.id)
+  setMovie(movie)
+  window.scrollTo(0,0)
 }
 
 //Busqueda de peliculas
@@ -50,19 +80,19 @@ useEffect(() => {
   return (
     <>
       <div>
-        <h2 className='text-center mt-5 mb-5'>Trailer Movies</h2>
+        <h2 className='text-center mt-5 mb-5'>Movie APP</h2>
         <form className='container mb-4' onSubmit={searchMovies}>
           <input type="text" placeholder='Search' onChange={(e)=> setSearchKey(e.target.value)} />
           <button className='btn btn-primary'>Search</button>
         </form>
         {/* En este contenedor se muestran las peliculas actuales */}
-        <div className='container mt-3'>
+        <div className='container mt-2'>
           <div className='row'>
             {
               movies.map((movie) =>(
                 <div key={movie.id} className='col-md-4 mb-3'>
                   <img src={`${URL_IMAGE + movie.poster_path}`} alt="" height={600} width="100%" />
-                  <h1 className='text-center'>{movie.title}</h1>
+                  <p className='text-center'>{movie.title}</p>
                 </div>
               ))
             }
