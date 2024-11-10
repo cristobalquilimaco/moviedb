4d5ee06cf955967296d66acff8d2be34dc76c5fe
Cristobal <quilimacox1@gmail.com>
2024-11-10 11:10:57 -0500

    Se inicia proyecto moviedb app

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import axios from 'axios'
+import YouTube from 'react-youtube'
+
+function App() {
+
+const API_URL = "https://api.themoviedb.org/3"
+const API_KEY = "0c8d154b2b93ccc0da064d6ff2a2575b"
+const IMAGE_PATH = "https://image.tmdb.org/t/p/original"
+
+
+  return (
+    <>
+      <div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
