7200cf761b53e7cccb64b4867eecf26556e93ad8
Cristobal <quilimacox1@gmail.com>
2024-11-10 20:18:58 -0500

    Se ajusta boton de menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,91 @@
-import "./App.css"
-import Home from "./Pages/Home"
+import { Route, Routes } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import Home from "./Pages/Home";
+import Navbar from "./Shared/Navbar";
 
 const App = () => {
-  return (
-    <div>
-      <Home/>
-    </div>
-  )
-}
+  const API_URL = "https://api.themoviedb.org/3";
+  const API_KEY = "0c8d154b2b93ccc0da064d6ff2a2575b";
 
-export default App
+  const [movies, setMovies] = useState([]);
+  const [searchKey, setSearchKey] = useState("");
+  const [trailer, setTrailer] = useState(null);
+  const [movie, setMovie] = useState({ title: "Loading Movies" });
+  const [playing, setPlaying] = useState(false);
+
+  // Función para obtener películas
+  const fetchMovies = async (searchKey) => {
+    const type = searchKey ? "search" : "discover";
+    const { data: { results } } = await axios.get(`${API_URL}/${type}/movie`, {
+      params: {
+        api_key: API_KEY,
+        query: searchKey,
+      },
+    });
+
+    setMovies(results);
+    setMovie(results[0]);
+
+    if (results.length) {
+      await fetchMovie(results[0].id);
+    }
+  };
+
+  // Función para obtener información de una película
+  const fetchMovie = async (id) => {
+    const { data } = await axios.get(`${API_URL}/movie/${id}`, {
+      params: {
+        api_key: API_KEY,
+        append_to_response: "videos",
+      },
+    });
+
+    if (data.videos && data.videos.results) {
+      const trailer = data.videos.results.find(
+        (vid) => vid.name === "Official Trailer"
+      );
+      setTrailer(trailer ? trailer : data.videos.results[0]);
+    }
+    setMovie(data);
+  };
+
+  // Función para manejar la búsqueda
+  const searchMovies = (e) => {
+    e.preventDefault();
+    fetchMovies(searchKey);
+  };
+
+  // Obtener las películas al cargar la app
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  return (
+    <>
+      <Navbar 
+        searchKey={searchKey} 
+        setSearchKey={setSearchKey} 
+        searchMovies={searchMovies} 
+      />
+      <Routes>
+        <Route 
+          index 
+          element={
+            <Home 
+              movies={movies} 
+              movie={movie} 
+              trailer={trailer} 
+              setPlaying={setPlaying} 
+              playing={playing} 
+              setMovie={setMovie}
+            />
+          } 
+        />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
---
+++ src/Shared/Navbar.jsx
@@ -0,0 +1,49 @@
+
+import "./styles/navbar.css";
+
+const Navbar = ({ searchKey, setSearchKey, searchMovies }) => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-transparent fixed-top" id="navbar">
+      <div className="container">
+        <a className="navbar-brand text-white" href="#">
+          Movie App
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo02"
+          aria-controls="navbarTogglerDemo02"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active text-white" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+          </ul>
+          <form className="d-flex" role="search" onSubmit={searchMovies}>
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+              value={searchKey}  // Vincula el input al estado searchKey
+              onChange={(e) => setSearchKey(e.target.value)} // Actualiza el searchKey al escribir
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
